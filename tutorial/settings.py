b4827457e5422c5412ef61699423f07b41e64a0f
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-11-25 15:35:36 +0300

    feat: (Tutorial: Quickstart)

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
