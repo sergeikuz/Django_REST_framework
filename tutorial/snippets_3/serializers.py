64cae3e333390cba6c601897311a9160acc776f2
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-11-26 17:54:25 +0300

    feat: Authentication & Permissions

--- tutorial/snippets_3/serializers.py
+++ tutorial/snippets_3/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 from tutorial.snippets_3.models import Snippet_3, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet_3.objects.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "snippets"]
 
 
 class SnippetSerializer_3(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
     class Meta:
         model = Snippet_3
-        fields = ["id", "title", "code", "linenos", "language", "style"]
-
+        fields = ["id", "title", "code", "linenos", "language", "style", 'owner']
--- tutorial/snippets_3/urls.py
+++ tutorial/snippets_3/urls.py
@@ -6,6 +6,8 @@ from tutorial.snippets_3 import views
 urlpatterns = [
     path("snippets_3/", views.SnippetList.as_view()),
     path("snippets_3/<int:pk>/", views.SnippetDetail.as_view()),
+    path("users/", views.UserList.as_view()),
+    path("users/<int:pk>/", views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
